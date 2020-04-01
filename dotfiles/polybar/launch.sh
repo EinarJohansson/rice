3b34b6b52b435d1df3ca7fd78ef6342eb44ff1d5
EinarJohansson <ejrobb09@gmail.com>
2020-04-01 19:46:37 +0200

    New directory structure

---
+++ dotfiles/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar middle >>/tmp/polybar1.log 2>&1 &
+polybar misc >>/tmp/polybar2.log 2>&1 &
