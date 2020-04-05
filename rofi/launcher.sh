7bb5179aca3c31d0d3fda9d907ed18924fa4bd8f
Einar Johansson <ejrobb09@gmail.com>
2020-04-05 16:36:16 +0200

    Delete launcher.sh

--- rofi/launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-## Author : Aditya Shakya (adi1090x)
-## Mail : adi1090x@gmail.com
-## Github : @adi1090x
-## Reddit : @adi1090x
-
-rofi -show drun -modi drun -theme ~/.config/rofi/style_icons_rainbow.rasi -disable-history
-
